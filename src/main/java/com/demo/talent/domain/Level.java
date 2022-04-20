ae9df39d4f3c2d8bdbc9fd9109301e4d7dee872b
Jinyoung Jang <jyjang@uengine.org>
2022-04-20 09:06:26 +0000

    value mapping for enum

--- src/main/java/com/demo/talent/domain/Level.java
+++ src/main/java/com/demo/talent/domain/Level.java
@@ -1,5 +1,18 @@
 package com.demo.talent.domain;
 
 public enum Level {
-  고수,  중수,  초급
+  고수(3)
+  ,  중수(2)
+  ,  하수(1);
+
+  private final int grade;
+
+  private Level(int grade){
+    this. grade = grade;
+  }
+
+  public int getGrade(){
+    return grade;
+  }
+
 }
